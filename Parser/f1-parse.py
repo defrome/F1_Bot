1c91b66e8bacc1dd9bb1fb969cb9d301c2c406be
DekaWz <oleinikovgeorge@gmail.com>
2025-04-11 19:22:28 +0500

    Абстрактные файлы для парсера, в self.link нужно добавить ссылку на сайт, потом доделаем парсинг

---
+++ Parser/f1-parse.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+class Parser():
+    def __init__(self) -> None:
+        self.link = ''
+        
+    
+    def _get_html(self, link: str | None = None) -> BeautifulSoup:
+        link = link or self.link
+
+        response = requests.get(link)
+        response.raise_for_status()
+
+        return BeautifulSoup(response.text, "html.parser")
+    
+
+    def get_team_status(self):
+        soup = self._get_html()
+        ...
+
+
+    def get_calendar(self):
+        soup = self._get_html()
+        ...
+
+
+    def __del__(self) -> None:
+        ...
