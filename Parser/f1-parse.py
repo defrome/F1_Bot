2655a5b7b0d3fdf09c9269017da3dfc2271d636d
DekaWz <oleinikovgeorge@gmail.com>
2025-04-11 21:58:51 +0500

    а

--- Parser/f1-parse.py
+++ Parser/f1-parse.py
@@ -8,25 +8,26 @@ class Parser():
 
 
     def _parse_table(self, html_table) -> list:
-
+        # обработка таблицы html
 
         def extract_cell_data(cell) -> list:
-            colspan = int(cell.get('colspan', 1))
-            rowspan = int(cell.get('rowspan', 1))
+            # достаёт данные из частитаблицы
 
-            return [cell.text] * colspan, rowspan
-        
-        
+            colspan = int(cell.get('colspan', 1)) # столбцы
+            rowspan = int(cell.get('rowspan', 1)) # строки
+
+            return [cell.text] * colspan, rowspan # возвращает элемент таблицы из строк и столбцов
 
 
-        table = []
+        table = [] # объявление возвращаемой таблицы
 
-        for row in html_table.find_all('tr'):
+        for row in html_table.find_all('tr'): # tr строка
 
             row_data = []
             row_spans = []
 
-            for cell in row.find_all(['th', 'td']):
+            for cell in row.find_all(['th', 'td']): # th и td столбцы, на сайте используется только td
+                
                 cell_data, cell_rowspan = extract_cell_data(cell)
                 row_data.extend(cell_data)
                 row_spans.extend([cell_rowspan] * len(cell_data))
@@ -37,19 +38,23 @@ class Parser():
                     if len(table) > 0:
                         row_data.insert(i, table[-1][i])
 
+            # добавление в таблицу данных
             table.append(row_data[1:])
         
+        # первый элемент таблицы это заголовок, он не нужен
         return table[1:]
 
 
 
     def _get_html(self, link: str | None = None) -> BeautifulSoup:
-        link = link or self.link
+        # создаёт объект парсера
 
-        response = requests.get(link)
-        response.raise_for_status()
+        link = link or self.link # если ссылка None, то использует self.link (она пока не определена)
 
-        return BeautifulSoup(response.text, "html.parser")
+        response = requests.get(link) # GET запрос на сайт
+        response.raise_for_status() # обработка ошибок как 403 и тп
+
+        return BeautifulSoup(response.text, "html.parser") # объект bs4 для парса по коду
     
 
 
@@ -61,13 +66,15 @@ class Parser():
 
 
     def get_calendar(self) -> list:
-        soup = self._get_html('https://f1calendar.com/')
+        # функция получает календарь и возвращает список из элементов календаря
 
-        html_table = soup.find('table')
+        soup = self._get_html('https://f1calendar.com/') # _get_html возвращает код страницы и тут же создаёт объект для парсера
 
-        calendar = self._parse_table(html_table)
+        html_table = soup.find('table') # ищет в html коде элементы класса <table>  
 
-        return [calendar[i:i+6] for i in range(0, len(calendar), 6)]
+        calendar = self._parse_table(html_table) # отправляет в парсер таблицу начиная с <table> заканчивая </table>
+
+        return [calendar[i:i+6] for i in range(0, len(calendar), 6)] # возвращает список из списков по 6 элементов
 
 
 
